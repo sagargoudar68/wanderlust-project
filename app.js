d7aa6d5045bf74f348b0950816a9a54354d2cb26
Sagar Goudar <sagargoudar293@gmail.com>
2026-06-30 00:11:52 +0530

    Updated Airbnb project

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-if(process.env.NODE_ENV !="production"){
+if (process.env.NODE_ENV != "production") {
   require("dotenv").config();
 }
 
+console.log("SECRET:", process.env.SECRET);
+console.log("DB URL:", process.env.ATLASDB_URL);
 
 const express = require("express");
 const app = express();
@@ -86,9 +88,9 @@ passport.deserializeUser(User.deserializeUser());
 
 
 app.use((req, res, next) => {
-  res.locals.success = req.flash("success") || [];
-  res.locals.error = req.flash("error") || [];
-  res.locals.currUser = req.user || null;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
   next();
 });
 
