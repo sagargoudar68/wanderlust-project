02ae5624d9b6edd5a3ef29b1639a3825ef7c6a6c
Sagar Goudar <sagargoudar293@gmail.com>
2026-06-29 22:16:34 +0530

    Fix flash and currUser undefined errors

--- app.js
+++ app.js
@@ -86,9 +86,9 @@ passport.deserializeUser(User.deserializeUser());
 
 
 app.use((req, res, next) => {
-  res.locals.success = req.flash("success");
-  res.locals.error = req.flash("error");
-  res.locals.currUser = req.user;
+  res.locals.success = req.flash("success") || [];
+  res.locals.error = req.flash("error") || [];
+  res.locals.currUser = req.user || null;
   next();
 });
 
